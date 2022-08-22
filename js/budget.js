5beeb414785ea7bfcb50d692055e151b6959ff0a
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2022-08-22 21:41:59 +0600

    'calculate added'

--- js/budget.js
+++ js/budget.js
@@ -1,10 +1,15 @@
+function perPlayearExpense() {
+  const perPlayer = document.getElementById("playerInput");
+  const perPlayearValue = parseInt(perPlayer.value);
+
+  const playearExpense = document.getElementById("total-player-expense");
+
+  playearExpense.innerText = perPlayearValue * 5;
+  perPlayer.value = "";
+}
+
 document
   .getElementById("playear-expense")
   .addEventListener("click", function () {
-    const perPlayer = document.getElementById("playerInput");
-    const perPlayearValue = parseInt(perPlayer.value);
-
-    const playearExpense = document.getElementById("total-player-expense");
-
-    playearExpense.innerText = perPlayearValue * 5;
+    perPlayearExpense();
   });
