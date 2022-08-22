27dc7e5e7c643bdd387d1c35786daeb1d844ffe2
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2022-08-22 21:15:50 +0600

    click function added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,35 @@
+function commonName(event) {
+  const messi = document.getElementById(event);
+  const value = messi.innerText;
+  return value;
+}
 
+document.getElementById("player-1").addEventListener("click", function () {
+  const player = commonName("messi");
+  const list = document.getElementById("list-1");
+  list.innerText = player;
+});
+
+document.getElementById("player-2").addEventListener("click", function () {
+  const player = commonName("ronaldo");
+  const list = document.getElementById("list-2");
+  list.innerText = player;
+});
+
+document.getElementById("player-3").addEventListener("click", function () {
+  const player = commonName("mane");
+  const list = document.getElementById("list-3");
+  list.innerText = player;
+});
+
+document.getElementById("player-4").addEventListener("click", function () {
+  const player = commonName("salah");
+  const list = document.getElementById("list-4");
+  list.innerText = player;
+});
+
+document.getElementById("player-5").addEventListener("click", function () {
+  const player = commonName("neymer");
+  const list = document.getElementById("list-5");
+  list.innerText = player;
+});
