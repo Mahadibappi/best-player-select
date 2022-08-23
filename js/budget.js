95f8ef5077374ab370f5afd6344326659fb607e2
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2022-08-23 15:27:44 +0600

    final calculation added

--- js/budget.js
+++ js/budget.js
@@ -1,15 +1,40 @@
-function perPlayearExpense() {
-  const perPlayer = document.getElementById("playerInput");
+function perPlayearExpense(event) {
+  const perPlayer = document.getElementById(event);
   const perPlayearValue = parseInt(perPlayer.value);
+  const TotalPlayerCost = perPlayearValue * 5;
 
   const playearExpense = document.getElementById("total-player-expense");
+  playearExpense.innerText = TotalPlayerCost;
 
-  playearExpense.innerText = perPlayearValue * 5;
   perPlayer.value = "";
 }
 
 document
   .getElementById("playear-expense")
   .addEventListener("click", function () {
-    perPlayearExpense();
+    perPlayearExpense("playerInput");
+  });
+
+// total cost calculation
+
+function finalCalculation() {
+  const managerCost = document.getElementById("manager-cost");
+  const managerValue = parseInt(managerCost.value);
+  managerCost.value = "";
+  const coachCost = document.getElementById("coach-cost");
+  const coachValue = parseInt(coachCost.value);
+  coachCost.value = "";
+  const managerCoachTotal = managerValue + coachValue;
+
+  const playearExpense = document.getElementById("total-player-expense");
+  const totalPlayerExpense = parseInt(playearExpense.innerText);
+  console.log(typeof totalPlayerExpense);
+
+  const totalCost = document.getElementById("total-cost");
+  totalCost.innerText = managerCoachTotal + totalPlayerExpense;
+}
+document
+  .getElementById("final-calculation")
+  .addEventListener("click", function () {
+    finalCalculation();
   });
